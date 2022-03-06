236d461f84a34116e5688b6663482d81e46aafa7
AR-LT <andrius.radziukynas@gmail.com>
2022-03-06 21:02:43 +0200

    finished  Task1

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,16 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const form = document.querySelector("form");
+const input = document.getElementById("search");
+const output = document.getElementById("output");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let lb = input.value * 2.2046;
+  let g = input.value / 0.001;
+  let oz = input.value * 35.274;
+  output.innerHTML = lb + " (lb)<br>" + g + " (g)<br>" + oz + " (oz)";
+  input.value = "";
+});
