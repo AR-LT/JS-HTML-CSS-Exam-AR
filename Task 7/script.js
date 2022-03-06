56e305bd6e207ddb85b334628a9d87ec401de358
AR-LT <andrius.radziukynas@gmail.com>
2022-03-06 23:50:19 +0200

    Task7

---
+++ Task 7/script.js
@@ -0,0 +1,19 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: "audi",
+  model: "A6",
+  year: 2005,
+  color: "white",
+};
+
+function showObjectKeys() {
+  let key = [];
+  for (let k in audi) key.push(k);
+  console.log(key);
+}
+showObjectKeys(audi);
